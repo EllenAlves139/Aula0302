d0087a2c0a46a2c5c744b558a1c5d1552a6be92a
Ellen <0001162817@senaimgaluno.com.br>
2026-02-11 10:25:05 -0300

    adisona o arquivo modificado

--- projeto0302/src/Componets/Carrossel.jsx
+++ projeto0302/src/Componets/Carrossel.jsx
@@ -3,7 +3,7 @@ import "/src/CSS/Carrossel.css"
 function Carrossel() {
   return (
     <div className='carrossel-container'>
-      <img src="https://ciencia.ufla.br/images/reportagens/2020/desmatadas-jpg.jpg" alt="" className='carrossel-image'/>
+      <img src="https://img.freepik.com/fotos-premium/um-monte-de-cameras-com-as-letras-x-sobre-eles_1279815-6042.jpg?semt=ais_hybrid&w=740&q=80" alt="" className='carrossel-image'/>
     </div>
   )
 }
