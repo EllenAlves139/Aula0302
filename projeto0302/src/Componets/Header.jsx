b90bf2d10b8b5149dd89873b16a3648a9ab81904
Ellen <0001162817@senaimgaluno.com.br>
2026-02-11 09:18:22 -0300

    adisona os arquivos modificados

--- projeto0302/src/Componets/Header.jsx
+++ projeto0302/src/Componets/Header.jsx
@@ -9,19 +9,19 @@ function Header() {
     </div>
     <nav className='nav-menu'>
         <a href="#inicio" className='nav-link'>
-           ("")
+           {""}
             Inicio
         </a>
              <a href="#sobre" className='nav-link' >
-           ("")
+           {""}
             Sobre
         </a>
              <a href="#produtos" className='nav-link'>
-           ("")
+         {""}  
             Produtos
         </a>
              <a href="#contato" className='nav-link'>
-           ("")
+           {""}
             Contato
         </a>
     </nav>
